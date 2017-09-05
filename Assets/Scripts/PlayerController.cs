f9ad2726bc8a84d083da1c98c37494da6f36d37a
Applepi7 <dhwjddnjs@naver.com>
2017-09-05 11:27:58 +0900

    Project basic setting and player moving

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 10;
+
+    private Rigidbody2D rigidbody;
+    private float moveHorizontal;
+    private float moveVertical;
+    private Vector2 movement;
+
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        moveHorizontal = Input.GetAxis("Horizontal");
+        moveVertical = Input.GetAxis("Vertical");
+
+        movement = new Vector2(moveHorizontal, moveVertical);
+
+        rigidbody.AddForce(movement * speed);
+	}
+}
